d0bd0c4cef0534ce4f10d3c099ee68a7c24676d1
Emmanuel Kippes <emmanuelkippes@gmail.com>
2017-06-18 19:34:37 -0300

    Se agregaron campos de Persona

--- Reservas/src/entidades/Persona.java
+++ Reservas/src/entidades/Persona.java
@@ -1,5 +1,55 @@
 package entidades;
 
 public class Persona {
+	private int id;
+	private int dni;
+	private String nombre;
+	private String apellido;
+	private String usuario;
+	private String password;
+	private boolean habilitado;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isHabilitado() {
+		return habilitado;
+	}
+	public void setHabilitado(boolean habilitado) {
+		this.habilitado = habilitado;
+	}
 
 }
