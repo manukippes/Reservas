7578214a41636ec4b08dc27022e5c70e55ce9689
Emmanuel Kippes <emmanuelkippes@gmail.com>
2017-09-03 18:37:05 -0300

    UI de Reservas la parte del cliente

--- Reservas/src/presentacion/ReservarElemento.java
+++ Reservas/src/presentacion/ReservarElemento.java
@@ -10,93 +10,88 @@ import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ReservarElemento extends JInternalFrame {
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField txtFechaHoraDesde;
+	private JTextField txtFechaHoraHasta;
+	private JTextField txtObservacion;
+	private JLabel lblSeleccioneTipoDe;
+	private JComboBox comboBoxElemento;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ReservarElemento frame = new ReservarElemento();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public ReservarElemento() {
 		setTitle("Reservar Elemento");
 		setClosable(true);
 		setMaximizable(true);
 		setIconifiable(true);
-		setBounds(100, 100, 732, 673);
+		setBounds(100, 100, 686, 561);
 		getContentPane().setLayout(new CardLayout(0, 0));
 		
 		JDesktopPane desktopPane = new JDesktopPane();
 		desktopPane.setBackground(Color.WHITE);
 		getContentPane().add(desktopPane, "name_40315080594877");
 		
-		JLabel lblSeleccioneTipoDe = new JLabel("Seleccione Tipo de Elemento a reservar:");
+///////////// Combo Tipo de Elemento///////////////////
+		lblSeleccioneTipoDe = new JLabel("Tipo de Elemento a reservar:");
 		lblSeleccioneTipoDe.setBounds(15, 36, 296, 20);
 		desktopPane.add(lblSeleccioneTipoDe);
+		JComboBox comboBoxTipoElemento = new JComboBox();
+		comboBoxTipoElemento.setBounds(255, 33, 398, 26);
+		desktopPane.add(comboBoxTipoElemento);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(307, 33, 398, 26);
-		desktopPane.add(comboBox);
-		
-		JLabel lblIndiqueFechaY = new JLabel("Indique Fecha y Hora de la reserva");
-		lblIndiqueFechaY.setBounds(15, 95, 263, 20);
-		desktopPane.add(lblIndiqueFechaY);
-		
-		textField = new JTextField();
-		textField.setBounds(61, 134, 227, 26);
-		desktopPane.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(346, 134, 146, 26);
-		desktopPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblHora = new JLabel("Hora");
-		lblHora.setBounds(303, 137, 54, 20);
-		desktopPane.add(lblHora);
-		
-		JLabel lblFecha = new JLabel("Fecha");
-		lblFecha.setBounds(15, 137, 54, 20);
+///////////// txt Fecha Hora Desde///////////////////
+		txtFechaHoraDesde = new JTextField();
+		txtFechaHoraDesde.setText("AAAA-MM-DD HH:MM:SS");
+		txtFechaHoraDesde.setBounds(255, 105, 393, 26);
+		desktopPane.add(txtFechaHoraDesde);
+		txtFechaHoraDesde.setColumns(10);
+		JLabel lblFecha = new JLabel("Fecha y Hora desde de la reserva");
+		lblFecha.setBounds(15, 108, 276, 20);
 		desktopPane.add(lblFecha);
 		
-		JLabel lblSeleccioneElementoA = new JLabel("Seleccione Elemento a Reservar:");
-		lblSeleccioneElementoA.setBounds(15, 228, 263, 20);
+///////////// txt Fecha Hora Hasta///////////////////
+		txtFechaHoraHasta = new JTextField();
+		txtFechaHoraHasta.setText("AAAA-MM-DD HH:MM:SS");
+		txtFechaHoraHasta.setBounds(255, 165, 393, 26);
+		desktopPane.add(txtFechaHoraHasta);
+		txtFechaHoraHasta.setColumns(10);
+		JLabel lblHora = new JLabel("Fecha y hora hasta de la reserva");
+		lblHora.setBounds(15, 168, 282, 20);
+		desktopPane.add(lblHora);
+
+///////////// Combo Elemento///////////////////		
+		JLabel lblSeleccioneElementoA = new JLabel("Elemento a Reservar:");
+		lblSeleccioneElementoA.setBounds(15, 231, 263, 20);
 		desktopPane.add(lblSeleccioneElementoA);
+		comboBoxElemento = new JComboBox();
+		comboBoxElemento.setBounds(255, 228, 398, 26);
+		desktopPane.add(comboBoxElemento);
 		
-		JButton btnBuscarElemento = new JButton("Buscar Elemento");
-		btnBuscarElemento.setBounds(273, 183, 179, 29);
-		desktopPane.add(btnBuscarElemento);
-		
-		textField_2 = new JTextField();
-		textField_2.setBounds(15, 473, 681, 79);
-		desktopPane.add(textField_2);
-		textField_2.setColumns(10);
-		
+///////////// txt Observacion///////////////////		
+		txtObservacion = new JTextField();
+		txtObservacion.setBounds(15, 333, 638, 79);
+		desktopPane.add(txtObservacion);
+		txtObservacion.setColumns(10);
 		JLabel lblObservaciones = new JLabel("Observaciones");
-		lblObservaciones.setBounds(15, 437, 121, 20);
+		lblObservaciones.setBounds(15, 297, 121, 20);
 		desktopPane.add(lblObservaciones);
 		
+///////////// Boton Reservar///////////////////		
 		JButton btnReservar = new JButton("Reservar");
-		btnReservar.setBounds(303, 587, 115, 29);
+		btnReservar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				reservar();
+			}
+		});
+		btnReservar.setBounds(298, 447, 115, 29);
 		desktopPane.add(btnReservar);
+		
+	}
+	
 
+	private void reservar() {
+		
+		
 	}
 }
